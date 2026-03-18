2a6bd61af4870ea14a8bc04cf42504b4cd312cb7
archiicrane <KellyCrane20@outlook.com>
2026-03-18 14:23:40 -0400

    image links updates test

--- images.js
+++ images.js
@@ -4,7 +4,7 @@ const archiveImages = [
     title: "Futurist Competition Drawing",
     year: 1972,
     tags: ["futurist", "retro", "utopian", "jetsons"],
-    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/5c2c96ad-e820-4f11-964f-6be228f4c4e8.jpg",
+    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/Alan+Hamilton+Rider_Campus+Plan_.jpg",
     description: "Speculative futuristic architectural drawing."
   },
   {
@@ -12,7 +12,7 @@ const archiveImages = [
     title: "Concept Board 02",
     year: 1984,
     tags: ["competition", "board", "graphic"],
-    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/YOUR-SECOND-IMAGE.jpg",
+    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/Alan+Hamilton+Rider_Dinning+Hall+Interior_.jpg",
     description: "Competition board with bold graphic layout."
   },
   {
@@ -20,7 +20,43 @@ const archiveImages = [
     title: "Urban Vision Sketch",
     year: 1968,
     tags: ["urban", "visionary", "linework"],
-    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/YOUR-THIRD-IMAGE.jpg",
+    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/Lloyd+G.+Walter+Jr._Cultural+center+plans_interior+perspective_.jpg",
+    description: "Visionary urban sketch with expressive linework."
+  },
+  
+  {
+    id: "img-004",
+    title: "Urban Vision Sketch",
+    year: 1968,
+    tags: ["urban", "visionary", "linework"],
+    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/Michael+James+Plautz_1967+Lloyd+Plans_Perspective+section.jpg",
+    description: "Visionary urban sketch with expressive linework."
+  },
+
+  {
+    id: "img-005",
+    title: "Urban Vision Sketch",
+    year: 1968,
+    tags: ["urban", "visionary", "linework"],
+    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/unknown_1968+lloyd+warrn+fellowship_site+plan%26section.jpg",
+    description: "Visionary urban sketch with expressive linework."
+  },
+
+  {
+    id: "img-006",
+    title: "Urban Vision Sketch",
+    year: 1968,
+    tags: ["urban", "visionary", "linework"],
+    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/Unknown_51st+paris_plan%26section.jpg",
+    description: "Visionary urban sketch with expressive linework."
+  },
+  
+  {
+    id: "img-007",
+    title: "Urban Vision Sketch",
+    year: 1968,
+    tags: ["urban", "visionary", "linework"],
+    imageUrl: "https://archivingresearch.s3.us-east-1.amazonaws.com/Unknown_A+center+for+people_s+consciousness_1st+half.jpg",
     description: "Visionary urban sketch with expressive linework."
   }
 ];
